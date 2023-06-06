1ccfbaf8c34b80e8b03ce8a4d52c0a24d3c5bca3
Aidan Aalund <aidanaalund@icloud.com>
2023-06-05 23:40:47 -0500

    Improved UI, added buttons for prediction (model not implemented)

--- webapp.py
+++ webapp.py
@@ -22,32 +22,30 @@ st.title("Predticker - A stock prediction webapp")
 if 'stocks' not in st.session_state:
     st.session_state.stocks = set(["AAPL", "GOOG", "MSFT", "GME"])
 
-# TODO: make the stocks a text input
+# User Input
+
 selected_stock = st.selectbox("Select ticker for prediction", 
                               st.session_state.stocks,
                               key='search_1')
+col1, col2 = st.columns([3,1])
 
 def addstock(newstock):    
     st.session_state.stocks.add(newstock)
     st.session_state.search_1 = newstock
     
-
-newstock = st.text_input(label='Add a ticker...', 
+with col1:
+    newstock = st.text_input(label='Add a ticker...', 
                          placeholder="Type a ticker and press enter to add to the list", 
                          max_chars=4,
                          value = 'AAPL')
+with col2:
+    st.write('')
+    st.write('')
+    adder = st.button('Add stock', on_click=addstock,
+                 args=(newstock, ))
 
-adder = st.button('Add stock', on_click=addstock,
-    args=(newstock, ))
-    
-    
-
-
-
-n_years = st.slider("Years of prediction:", 1, 4)
-period = n_years * 365
-
-#we can use st.cache_resource for ML models later on!
+# Loading data
+# we can use st.cache_resource for ML models later on!
 @st.cache_data
 def load_data(ticker):
     data = yf.download(ticker,START,TODAY)
@@ -59,7 +57,7 @@ data_load_state = st.text("Loading data...")
 data = load_data(selected_stock)
 data_load_state.text("Finished!")
 
-# This section removes gaps in the plot by creating a list of missing dates
+# Data preprocessing
 # grab first and last observations from df.date and make a continuous date range from that
 dt_all = pd.date_range(start=data['Date'].iloc[0],end=data['Date'].iloc[-1], freq = 'D')
 
@@ -69,27 +67,23 @@ dt_obs = [d.strftime("%Y-%m-%d") for d in data['Date']]
 # isolate missing timestamps
 dt_breaks = [d for d in dt_all.strftime("%Y-%m-%d").tolist() if not d in dt_obs]
 
+# For debugging, this will display the last 5 rows of our dataframe
+#st.subheader("Raw Data")
+#st.write(data.tail())
 
-st.subheader("Raw Data")
-st.write(data.tail())
-
-# TODO: add volume plot with this selector
-# col1, col2, col3, col4, col5, col6, col7, col8 = st.columns([3.5,1,1,1,1,1,1,1])
-# buttonNames = ['1D', '5D', '1M', '6M', 'YTD', '1Y', '5Y']
-# buttonDictionary = {
-#     '1D' : date.today - timedelta(days = 1), 
-#     '5D' : date.today - timedelta(days = 5), 
-#     '1M' : date.today - timedelta(weeks = 4), 
-#     '6M' : date.today - timedelta(weeks = 24), 
-#     'YTD' : startOfYear,
-#     '1Y' : date.today - timedelta(days = 365), 
-#     '5Y' : date.today - timedelta(days = 365 * 5)
-#     }
-# buttons = []
+#TODO: machine learning goes here :)
+col3, col4 = st.columns([3,1])
+with col3:
+    n_years = st.slider("Years of prediction:", 1, 4)
+    
+period = n_years * 365
+with col4:
+    st.write('')
+    st.write('')
+    adder = st.button('Predict... :male_mage:')
 
 
-# define the plot types and the default layout to them
-
+# Define the plot types and the default layout to them
 candlestick = go.Candlestick(x=data['Date'], open=data['Open'], 
                              high=data['High'], low=data['Low'], 
                              close=data['Close'])
@@ -144,7 +138,7 @@ stocklayout = dict(
         ]
         ) 
     )
-
+# Display candlestick and volume plots
 fig = go.Figure(data=candlestick, layout=stocklayout)
 fig.update_layout(title = 'Candlestick Plot')
 fig2 = go.Figure(data=volume, layout=stocklayout)
@@ -152,31 +146,3 @@ fig2.update_layout(title = 'Volume Plot')
 
 st.plotly_chart(fig)
 st.plotly_chart(fig2)
-
-# with col1:
-#     graphtypes = ['Candlestick Plot', 'Volume']
-#     graph_selector = st.selectbox("Select graph type", graphtypes)
-# with col2:
-#     buttons.append(st.button('1D'))
-# with col3:
-#     buttons.append(st.button('5D'))
-# with col4:
-#     buttons.append(st.button('1M'))
-# with col5:
-#     buttons.append(st.button('6M'))
-# with col6:
-#     buttons.append(st.button('YTD'))
-# with col7:
-#     buttons.append(st.button('1Y'))
-# with col8:
-#     buttons.append(st.button('5Y'))
-    
-# for button in enumerate(buttons):
-#     if button:
-#         plot_raw_data(buttonDictionary.get(button.label))
-        
-
-
-
-
-
