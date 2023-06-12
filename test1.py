4127c4c64a5ec1305c4cb845f8bf92a31ba12c5d
Aidan Aalund <aidanaalund@icloud.com>
2023-06-12 00:15:15 -0500

    Graph Overhaul + Test files
    Overhauled the look and feel of the graphs, including dynamic appearance based on day over day closing price trends. Added some test files for use in model training.

---
+++ test1.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  8 16:48:21 2023
+
+@author: aidan
+"""
+
+import yfinance as yf
+import pandas as pd
+from datetime import date, timedelta
+
+# START = "2019-01-01"
+# TODAY = date.today().strftime("%Y-%m-%d")
+# stock_data = yf.download('NFLX',START,TODAY)
+# print(stock_data.head())
+# print(stock_data.info())
+# stock_data.reset_index(inplace=True)
+# stock_data['Date'] = pd.to_datetime(stock_data['Date'])
+
+# print(stock_data.head())
+
+stock_data = pd.read_csv('NFLX.csv',index_col='Date')
+print(stock_data.head())
+
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import datetime as dt
+
+# plt.figure(figsize=(15,10))
+# # This formates dates in matplotlib
+# plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+# plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=120))
+# x_dates = [dt.datetime.strptime(d,'%Y-%m-%d').date() for d in stock_data.index.values]
+
+# plt.plot(x_dates, stock_data['High'], label='High')
+# plt.plot(x_dates, stock_data['Low'], label='Low')
+# plt.xlabel('Time Scale')
+# plt.ylabel('Scaled USD')
+# plt.legend()
+# plt.gcf().autofmt_xdate()
+# plt.show()
+
+# import numpy as np
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense
+# from tensorflow.keras.layers import LSTM
+# from tensorflow.keras.layers import Dropout
+# from tensorflow.keras.layers import *
+# from tensorflow.keras.callbacks import EarlyStopping
+
+# from sklearn.preprocessing import MinMaxScaler, StandardScaler
+# from sklearn.metrics import mean_squared_error
+# from sklearn.metrics import mean_absolute_percentage_error
+# from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import TimeSeriesSplit
+# from sklearn.metrics import mean_squared_error
+
+# target_y = stock_data['Close']
+# X_feat = stock_data.iloc[:,0:3]
+
+# #Feature scaling
+# sc = StandardScaler()
+# X_ft = sc.fit_transform(X_feat.values)
+# X_ft = pd.DataFrame(columns=X_feat.columns,
+#                         data=X_ft,
+#                         index=X_feat.index)
+
+# def lstm_split(data, n_steps):
+#     x, y = [], []
+#     for i in range(len(data)-n_steps+1):
+#         x.append(data[i:i + n_steps, :-1])
+#         y.append(data[i + n_steps-1, -1])
+#     return np.array(x), np.array(y)
+
+# # Unsure if the following lines before the print are correct
+# X1, y1 = lstm_split(X_ft.values, n_steps=2)
+
+# train_split = 0.8
+# split_idx =  int(np.ceil(len(X1)*train_split))
+# date_index = X_ft.index
+
+# X_train, X_test = X1[:split_idx], X1[split_idx:]
+# y_train, y_test = y1[:split_idx], y1[split_idx:]
+# X_train_date, X_test_date = date_index[:split_idx], date_index[split_idx:]
+
+# print(X1.shape, X_train.shape, X_test.shape, y_test.shape)
+
+# lstm = Sequential()
+# lstm.add(LSTM(32, input_shape=(X_train.shape[1],X_train.shape[2]),
+#               activation='relu', return_sequences=True))
+# lstm.add(Dense(1))
+# lstm.compile(loss='mean_squared_error', optimizer='adam')
+# lstm.summary()
+
+# history = lstm.fit(X_train, y_train,
+#                    epochs=100, batch_size=4,
+#                    verbose=2, shuffle=False)
+
+# y_pred = lstm.predict(X_test)
+
+
+# # rmse = mean_squared_error(y_test, y_pred, squared=False)
+# # mape= mean_absolute_percentage_error(y_test, y_pred)
+# # print("RSME: ", rmse)
+# # print("MAPE: ", mape)
+
+
+# # plt.plot(x_dates, stock_data['Close'], label='True Close')
+# # plt.plot(x_dates, y_pred, label='LSTM Value')
+# # plt.xlabel('Time Scale')
+# # plt.ylabel('USD')
+# # plt.legend()
+# # plt.gcf().autofmt_xdate()
+# # plt.show()
+
+
+
+
+
+
+
+
+
--- webapp.py
+++ webapp.py
@@ -19,7 +19,7 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
 from collections import deque
 
-START = "2022-01-01"
+START = "2023-05-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 YEAR = date.today().strftime("%Y")
 startOfYear = "f'{YEAR}-01-01'"
@@ -47,7 +47,7 @@ with title:
 # with loadingtext:
 #     #data_load_state = st.markdown("(_Loading data..._)")
 
-st.subheader("A magic stock predictor & dashboard") 
+st.caption("A magic stock predictor & dashboard") 
     
 if 'stocks' not in st.session_state:
     st.session_state.stocks = set(["AAPL", "GOOG", "MSFT", "GME"])
@@ -79,7 +79,7 @@ with col3:
 
 # Loading data
 # we can use st.cache_resource for ML models later on!
-@st.cache_data
+@st.cache_data(show_spinner=False)
 def load_data(ticker):
     data = yf.download(ticker,START,TODAY)
     data.reset_index(inplace=True)
@@ -98,10 +98,8 @@ if 'currentdataframe' not in st.session_state:
 # Data preprocessing
 # grab first and last observations from df.date and make a continuous date range from that
 dt_all = pd.date_range(start=data['Date'].iloc[0],end=data['Date'].iloc[-1], freq = 'D')
-
 # check which dates from your source that also accur in the continuous date range
 dt_obs = [d.strftime("%Y-%m-%d") for d in data['Date']]
-
 # isolate missing timestamps
 dt_breaks = [d for d in dt_all.strftime("%Y-%m-%d").tolist() if not d in dt_obs]
 
@@ -167,7 +165,6 @@ def predict(stockdataframe):
     if st.session_state.predictiontext == '':
         st.session_state.predictiontext = 'Please input the stock again!'  
         
-
     #take the dataframe, chop it the amount back specified internally
     NUMBER_of_STEPS_BACK = 7 # Number of days back that the model will be trained for
     #TODO: allow PREDICTION_STEPS to be modified based on the slider.
@@ -201,9 +198,8 @@ def predict(stockdataframe):
     if len(predictions) > 0:
       predictions_list = [str(d)+'$' for d in predictions]
       predictions_str = ', '.join(predictions_list)
-      message = f":sparkles: {selected_stock}'s share price prediction for the next day(s) is {predictions_str} :sparkles:"
+      message = f":sparkles: {selected_stock}'s closing price prediction(s): {predictions_str} :sparkles:"
       st.session_state.predictiontext = message
-          
 
 
 # PREDICTION UI
@@ -217,8 +213,9 @@ with col4:
     st.write('')
     adder = st.button(f'Predict **{n_years}** day(s) ahead... :male_mage:')
 with col5:
+    st.write('')
+    #prediction = st.write(st.session_state.predictiontext)
     if adder:
-        st.write('')
         st.write('')
         st.write('')
         with st.spinner('Predicting...'):
@@ -286,14 +283,50 @@ stocklayout = dict(
 
 # Display candlestick and volume plots
 fig = go.Figure(data=candlestick, layout=stocklayout)
-fig.update_layout(title = 'Candlestick Plot', yaxis_title='Share Price ($)')
+fig.update_layout(title = '', 
+    yaxis_title='Share Price ($)',
+    autosize=False,
+    width=700,
+    height=400,
+    margin=dict(
+        l=0,
+        r=10,
+        b=30,
+        t=10,
+        pad=4
+    ),)
 fig2 = go.Figure(data=volume, layout=stocklayout)
-fig2.update_layout(title = 'Volume Plot',yaxis_title='Number of Shares')
+fig2.update_layout(title = 'Volume Plot',yaxis_title='Number of Shares',
+                   autosize=False,
+                   width=700,
+                   height=400,
+                   margin=dict(
+                       l=0,
+                       r=10,
+                       b=100,
+                       t=75,
+                       pad=4
+                   ),)
 
-plotcontainer = st.container()
-volume, candle = plotcontainer.columns([4,4])
-#TODO: have some statistics like the google dashboard.
-with volume:
-    plotcontainer.plotly_chart(fig)
-with candle:
-    plotcontainer.plotly_chart(fig2)
+header, subinfo = st.columns([2,4])
+change = data['Close'].iloc[-1] - data['Close'].iloc[-2]
+#TODO: this logic might be incorrect. Is it day over day?
+with header:
+    price = data['Close'].iloc[-1]
+    #DoD increase, percentage increase, arrow
+
+    percentage = (float(data['Close'].iloc[-1] - data['Close'].iloc[-2])/abs(data['Close'].iloc[-2]))*100.00
+    if change > 0:
+        st.header('${:0.2f}'.format(price)+':chart_with_upwards_trend:')
+        string = '+${:0.2f}'.format(change)+'  (+{:0.2f}'.format(percentage)+'%)'
+        st.caption(f':green[{string}]')
+    elif change == 0:
+        st.header('${:0.2f}'.format(price)+':arrow_right:')
+        st.caption('+${:0.2f}'.format(change)+'  (+{:0.2f}'.format(percentage)+'%)')
+    else:
+        st.header('${:0.2f}'.format(price)+':chart_with_downwards_trend:')
+        string = '-${:0.2f}'.format(abs(change))+'  (-{:0.2f}'.format(abs(percentage))+'%)'
+        st.caption(f':red[{string}]')
+    
+st.plotly_chart(fig)
+st.plotly_chart(fig2)
